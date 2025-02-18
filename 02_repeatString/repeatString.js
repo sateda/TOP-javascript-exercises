179c8862c0d5710772b61dccfb7fcbb7b314e888
sateda <7441698+sateda@users.noreply.github.com>
2025-02-18 21:05:52 +0100

    Finish exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,25 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatString = string;
+    let count = num;
+    
+     // Loop to display the string * num
+    for(let i=1; i < count; i++) {
+        repeatString = repeatString + string;
+        if(i === num) {
+            
+        }
+    }
 
+    console.log(count);
+    console.log(num);
+
+    if(num === 0) {
+        return string = "";
+    } else if(num < 0) {
+        return string = "ERROR";
+    } else {
+        return repeatString;
+    }
 };
 
 // Do not edit below this line
