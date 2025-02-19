9f0d77ca5d57b84e20fa7f71763fb0ea9adc9ed6
sateda <7441698+sateda@users.noreply.github.com>
2025-02-19 20:53:46 +0100

    Complete exercise 03_reverseString, TODO refactor and comment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,28 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Convert string to an array
+    string = string.split("");
+    
+    let num = string.length;
+    let reverseString = string.reverse();
+    //string = reverseString.toString();
+    //string = string.replaceAll(",", "");
+    
+    string = "";
 
+    reverseString.forEach(addToString);
+
+    function addToString(value) {
+        string = string + value;
+    }
+
+    return string;
+
+    // for(i=num; i<0; i--) {
+    //     reverseString    
+    // }
+
+
+    console.log(string);
 };
 
 // Do not edit below this line
