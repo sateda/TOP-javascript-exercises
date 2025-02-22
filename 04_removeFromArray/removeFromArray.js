96ff30c1c1145a39368abc4994a3cd638d1bf8bb
sateda <7441698+sateda@users.noreply.github.com>
2025-02-22 13:38:07 +0100

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(removeArray, ...manyMoreArgs) {
+
+// Counter variable to keep track of the manyMoreArgs[num];
+let num=0;
+
+/* Return the new array */
+function checkElement(element) {
+    return element !== manyMoreArgs[num];
+}
+
+// Loop all the elements that need to be removed from the given string
+for(const value of manyMoreArgs) {
+    removeArray = removeArray.filter(checkElement);
+    num++;
+}
+
+return removeArray;
+
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//   return string.split("").reverse().join("");
