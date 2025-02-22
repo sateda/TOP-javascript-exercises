ee520fb2302c2dd2b9025dc134d4c3d04a044713
sateda <7441698+sateda@users.noreply.github.com>
2025-02-22 22:04:43 +0100

    Refactored == to ===

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
     // Divisible bij 4 is True unless divisible by 100 except when it's divisible by 400
-    if(year % 4 == 0 && (!(year % 100 == 0) || year % 400 == 0)) return true;
+    if(year % 4 === 0 && (!(year % 100 === 0) || year % 400 === 0)) return true;
     return false;
 };
 
