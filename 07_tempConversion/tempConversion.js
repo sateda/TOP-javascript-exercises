c0cc670fb0c95c738d23a44905f46500bb608051
sateda <7441698+sateda@users.noreply.github.com>
2025-02-22 22:28:26 +0100

    Complete assignment 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  celsius = (Fahrenheit-32)/1.8;
+  celsius = Math.round(celsius * 10)/10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = (celsius * 1.8) + 32;
+  fahrenheit = Math.round(fahrenheit *10)/10;
+  return fahrenheit
 };
 
 // Do not edit below this line
