78b484a01300a12f52bf7eed2c43c695543af24a
sateda <7441698+sateda@users.noreply.github.com>
2025-03-11 20:54:53 +0100

    complete assignment 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  let result = 1;
+  for(let i=1; i<=factorial; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
