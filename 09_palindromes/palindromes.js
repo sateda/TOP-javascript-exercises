8217a837d34476759d018776424593b17eba7cfd
sateda <7441698+sateda@users.noreply.github.com>
2025-03-13 19:51:10 +0100

    Complete assignment 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let removedPunctuations = text.replace(/,|\.|!| /g,"").toLowerCase();
+    const reverseText = removedPunctuations.split("").reverse().join("");
+    if(removedPunctuations == reverseText) { 
+        return true; 
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line
