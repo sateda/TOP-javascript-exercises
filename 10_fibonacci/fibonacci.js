40bd2e07aace2a29bf78e02d179e45a62e279134
sateda <7441698+sateda@users.noreply.github.com>
2025-03-13 20:23:19 +0100

    Solved exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    const fibonacciNumber = parseInt(a);
+    
 
+    if (fibonacciNumber < 0) return "OOPS";
+    if (fibonacciNumber === 0) return 0;
+
+    let counter = [0, 1];
+    for(let i=2; i <= fibonacciNumber; i++) {
+        counter.push(counter[counter.length-2] + counter[counter.length-1]);
+    }
+
+    return counter[counter.length-1];
 };
 
 // Do not edit below this line
